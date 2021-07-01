32fa6e6e2a265bf9b53cf6d954bca4346c77f5a0
md <deeplearnmd@gmail.com>
2021-07-01 16:29:41 +0100

    basic auth

--- anna/app.py
+++ anna/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
+from functools import wraps
 import os
 import pandas as pd
 from anna.content import DataSet, get_global_vars
@@ -6,7 +7,31 @@ from anna.utils import switch_button_state
 
 app = Flask(__name__)
 
+#### BASIC AUTH
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'bobby86' and password == 'dsileinv23sdfljklsein2304AsdfOJdmseo7431sdf'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route("/")
+@requires_auth
 def home0():
     # Load list_files and list_configs as global variables
     list_configs, list_files = get_global_vars(app.config['INPUT_PATH'], app.config['CONFIG_FILE_PATH'])
@@ -35,6 +60,7 @@ def home0():
             ds = ds, )
 
 @app.route("/annotate/<string:config_name>/<string:file_name>")
+@requires_auth
 def home(file_name,config_name):
     list_configs, list_files = get_global_vars(app.config['INPUT_PATH'], app.config['CONFIG_FILE_PATH'])
     ds = DataSet(file_name, app.config['INPUT_PATH'], app.config['ANNOTATIONS_PATH'], config_name, app.config['CONFIG_FILE_PATH'])
@@ -55,6 +81,7 @@ def home(file_name,config_name):
 
 ### ADD LINE
 @app.route("/add_line/<string:config_name>/<string:file_name>/<string:dp_id>")
+@requires_auth
 def add_line(dp_id, file_name, config_name):
     list_configs, list_files = get_global_vars(app.config['INPUT_PATH'], app.config['CONFIG_FILE_PATH'])
     ds = DataSet(file_name, app.config['INPUT_PATH'], app.config['ANNOTATIONS_PATH'], config_name, app.config['CONFIG_FILE_PATH'])
@@ -75,6 +102,7 @@ def add_line(dp_id, file_name, config_name):
 
 ### LABELLING
 @app.route("/label/<string:label_type>/<string:config_name>/<string:file_name>/<string:label_name>/<string:dp_id>", methods=['POST'])
+@requires_auth
 def update(label_name, dp_id, file_name, config_name, label_type):
     received_url = request.form['url']
     received_label_name = request.form['label_name']
@@ -95,6 +123,7 @@ def update(label_name, dp_id, file_name, config_name, label_type):
 
 ### ADDING COMMENTS
 @app.route("/comment/<string:label_type>/<string:config_name>/<string:file_name>/<string:dp_id>", methods=['POST'])
+@requires_auth
 def add_comment(dp_id, file_name, config_name, label_type):
     comment = request.form.get("comment_field")
     print(label_type)
@@ -107,6 +136,7 @@ def add_comment(dp_id, file_name, config_name, label_type):
 
 ### CONTENT EDITING - EDIT
 @app.route("/content_edits/<string:config_name>/<string:file_name>/<string:dp_id>", methods=['POST'])
+@requires_auth
 def edit_text(dp_id, file_name, config_name):
     comment = request.form.get("comment_field").strip()
     received_url = request.form['url']
@@ -125,6 +155,7 @@ def edit_text(dp_id, file_name, config_name):
 
 ### CONTENT EDITING - RESET EDIT
 @app.route("/remove_edits/<string:config_name>/<string:file_name>/<string:dp_id>", methods=['POST'])
+@requires_auth
 def remove_edits(dp_id, file_name, config_name):
     received_url = request.form['url']
     new_url = received_url.replace('remove_edits','content_edits')
